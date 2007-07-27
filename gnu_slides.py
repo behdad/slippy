485cf7ec8fb67078ed982bf46b52ce830dd43132
Behdad Esfahbod <behdad@behdad.org>
2007-07-26 21:58:40 -0400

    Finish GNU example

--- gnu_slides.py
+++ gnu_slides.py
@@ -14,12 +14,14 @@ def text_slide (l):
 			yield ''
 	slide_add (s, data={'align': pango.ALIGN_LEFT})
 
-text = ["""“Free software” is a matter of liberty, not price. To understand the 
+texts = {}
+texts['en'] = """“Free software” is a matter of liberty, not price. To understand the 
 concept, you should think of “free” as in “free speech”, not as in “free beer.”
 
 Free software is a matter of the users' freedom to run, copy, 
-distribute, study, change and improve the software.""",
-"""More precisely, it refers to four kinds of freedom, for the users of the 
+distribute, study, change and improve the software.
+
+More precisely, it refers to four kinds of freedom, for the users of the 
 software:
 ------------------------------------------------------------------------------
 0. The freedom to run the program, for any purpose.
@@ -29,16 +31,85 @@ software:
 3. The freedom to improve the program, and release your improvements to 
    the public, so that the whole community benefits.
    Access to the source code is a precondition for this.
-------------------------------------------------------------------------------""",
-"""The concept of these 4 freedoms (0-3) were developed by Richard 
-Stallman.
+------------------------------------------------------------------------------
+The concept of these 4 freedoms (0-3) were developed by Richard Stallman.
 
 To set a good example he started to write a completely free operating system.
 
-Today Linux based GNU systems are used by millions of people around 
-the world."""]
+Today Linux based GNU systems are used by millions of people around the world."""
 
-for slide in text:
+texts['de'] = """Bei dem Begriff „Freie Software“ geht es um Freiheit, nicht um den Preis.
+Um dieses Konzept richtig begreifen zu können, sollte man an „frei“ wie in
+„freie Rede“ denken, und nicht an „Freibier“.
+
+Bei „Freier Software“ geht es um die Freiheit des Benutzers die Software nach
+Belieben zu benutzen, zu kopieren, weiter zu geben, die Software zu studieren,
+sowie Änderungen und Verbesserungen an der Software vornehmen zu können.
+------------------------------------------------------------------------------
+Genauer gesagt, bezieht sich der Begriff „Freie Software“ auf vier Arten von
+Freiheit, die der Benutzer der Software hat:
+
+0. Die Freiheit, das Programm für jeden Zweck zu benutzen.
+1. Die Freiheit, zu verstehen, wie das Programm funktioniert und wie man es
+   für seine Ansprüche anpassen kann.
+   Der Zugang zum Quellcode ist dafür Voraussetzung.
+------------------------------------------------------------------------------
+2. Die Freiheit, Kopien weiterzuverbreiten, so dass man seinem Nächsten
+   weiterhelfen kann.
+3. Die Freiheit, das Programm zu verbessern und die Verbesserungen der
+   Allgemeinheit zur Verfügung zu stellen, damit die ganze Gemeinschaft davon
+   profitieren kann.
+   Der Zugang zum Quellcode ist dafür Voraussetzung.
+------------------------------------------------------------------------------
+Diese 4 Freiheiten (0-3) wurden so von Richard Stallman entworfen.
+
+Um mit gutem Beispiel voran zu gehen, hat er angefangen, ein vollständig
+freies Betriebssystem zu entwickeln.
+
+Heute werden Linux basierte GNU Systeme von vielen Millionen Anwendern benutzt."""
+
+texts['he'] = """"תוכנה חופשית" זה ענײן של חירות, לא של מחיר. כדי להבין את העקרון,
+צריך לחשוב על "חופש" כמו ב"חופש הביטוי"...\
+.effectpause
+.back 3
+ ולא כמו ב"בירה חופשי".
+
+תוכנה חופשית נוגעת לחופש של משתמשים להריץ, להפיץ הפצת-המשך, ללמוד, 
+לשנות ולשפר את התוכנה. ליתר דיוק, זה מתײחס לארבעה סוגים של חירות למשתמשי
+התוכנה:
+------------------------------------------------------------------------------
+0. החופש להריץ את התוכנה, לכל מטרה שהיא.
+1. החופש ללמוד איך תוכנה עובדת, ולשנות אותה לצרכיהם.
+   גישה לקוד המקור היא תנאי מקדים לכך.
+2. החופש להפיץ עותקים בהפצה-חוזרת כדי שיוכלו למשל לעזור לשכנים שלהם.
+3. החופש לשפר את התוכנה, ולשחרר את השיפורים שלהם לציבור, כך שכל הקהילה תרויח.
+   גישה לקוד-המקור היא תנאי מקדים לכך.
+------------------------------------------------------------------------------
+The concept of these 4 freedoms (0-3) were developed by Richard Stallman.
+
+To set a good example he started to write a completely free operating system.
+
+Today Linux based GNU systems are used by millions of people around the world."""
+
+import os, re
+lang = os.getenv ('LANG')
+i = lang.find ('_')
+if i > 0:
+	lang = lang[:i]
+text = texts.get (lang, texts['en'])
+def break_on_dashlines (text):
+	s = ''
+	for line in text.split ('\n'):
+		if re.match ('^----*$', line):
+			yield s
+			s = ''
+		else:
+			if s:
+				s += '\n'
+			s += line
+	yield s
+		
+for slide in break_on_dashlines (text):
 	text_slide (slide)
 
 if __name__ == "__main__":
